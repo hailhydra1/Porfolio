0002f742f7cbb3e5bf268eda2d0a6d9c6ffef32f
Pranay <pranaysingh303@gmail.com>
2024-01-19 04:10:38 +0530

    correction

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -45,8 +45,7 @@ const Projects = () => {
               imgPath={kickstart}
               isBlog={false}
               title="UMotel"
-              description="This Hotel Booking Platform utilizes a REST API built with Nodejs and Expressjs, allowing for easy CRUD operations on Hotel booking and addition from the admin panel. Multiple images and perks and pricing can be added at the time of creation of the hotel. Users can also see actual location of the website on google maps. The website uses MongoDB and AmazonS3 Buckets for the database."
-              ghLink="https://github.com/19sajib/mern-stack-ecommerce"
+              description="https://github.com/hailhydra1/pranay-booking-app"
             />
           </Col>
 
