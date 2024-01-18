a99c8aa95aab5395d264bfbcf8274fdaab0ad914
Pranay <pranaysingh303@gmail.com>
2024-01-19 02:24:11 +0530

    resume fix

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/Resume.pdf`
+// const resumeLink = `https://raw.githubusercontent.com/hailhydra1/Portfolio/main/src/assets/Resume.pdf`
 
 
 const Resume = () => {
@@ -39,7 +39,7 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
